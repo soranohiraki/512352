9771d727205a1636fff1db45e02017e392f0e734
sungpado <34066247+sungpado@users.noreply.github.com>
2018-02-03 19:31:43 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const yt = require('ytdl-core');
+const client = new Discord.Client()
+const config = require("./config.json");
+const embed = new Discord.RichEmbed()
+
+client.on('ready', () => {
+  client.user.setGame(`노래봇입니다!`);
+});
+
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`노래봇입니다!`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setGame(`노래봇입니다!`);
+});
+
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === "핑") {
+    
+     message.delete().catch(O_o=>{});
+   
+    const m = await message.channel.send("핑이요?");
+    m.edit(`퐁! 대기 시간은 ${m.createdTimestamp - message.createdTimestamp}ms 입니다. API 대기 시간은 ${Math.round(client.ping)}ms 입니다. ^^7`);
+  }
+
+
+ });
+
+client.login(process.env.BOT_TOKEN);
